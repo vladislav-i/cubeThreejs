5e1426b327e95118142de786ec8666cc80c420bf
vladislav-i <saintmz3@gmail.com>
2015-03-19 19:54:24 -0700

    changed it from plane into a cube

--- javascript.js
+++ javascript.js
@@ -10,17 +10,20 @@ var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHei
 //set position
 
 //starting at center point 
-camera.position.y= -450; //y position, new and far
-camera.position.z=400; //z is up and down
-camera.rotation.x= .75; //left and right
+camera.position.y= -400; //y position, new and far
+camera.position.z= 400; //z is up and down
+camera.rotation.x= .70; //left and right
 
 var scene = new THREE.Scene(); //creat scene
 
-//normal material for mesh
-var plane = new THREE.Mesh(new THREE.PlaneGeometry(300,300), new THREE.MeshNormalMaterial());
+//normal material for mesh, creating cube
+//cube has x,y,z = all set to 200
+var cube  = new THREE.Mesh(new THREE.CubeGeometry(200,200,200), new THREE.MeshNormalMaterial());
 
+//rotating cube 45deg.
+cube.rotation.z = .5;
 //adding plane to scene
-scene.add(plane);
+scene.add(cube);
 //rendere this scene with this camera
 renderer.render(scene, camera);
 };

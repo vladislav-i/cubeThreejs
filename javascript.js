6058ec020a5b46d60d0f0a9ae4f0f81d160293f0
vladislav-i <saintmz3@gmail.com>
2015-04-02 19:19:30 -0700

    creating multiple cubes/ rotating camera around cubes

--- javascript.js
+++ javascript.js
@@ -1,65 +1,81 @@
 
 //need window on load to execute javascript
 window.onload = function() {
+	var camera, controls, scene, renderer;
+	init();
+	animate();
+	function init(){
+		//set camera
+	    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
+	    camera.position.x = 500;
+	    //user trackballcontrols (tracks our camera)
+	    controls = new THREE.TrackballControls(camera);
+	    controls.addEventListener('change', render);
+	    //creating scene
+	    scene = new THREE.Scene();
+	    
+	    //create cube
+	    var geometry = new THREE.CubeGeometry(100,100,100);
+	    //create material 
+	    var material = new THREE.MeshNormalMaterial();
 
-var angularSpeed = 0.2 // rotations per second 
-var lastTime =  0; // once animate runs, time will change
-
-function animate(){
-	//get time
-	var time = (new Date).getTime();
-	var timeDiff = time - lastTime; //get difference in time
-	var angularChange = angularSpeed * timeDiff * 2 * Math.PI/1000;
-
-	cube.rotation.z += angularChange; // taking current position and moving it
-	cube.rotation.x += angularChange;
-	cube.rotation.y += angularChange;
-
-	lastTime = time; // update current time
-
-	//rendere this scene with this camera
-	renderer.render(scene, camera); 
-
-	requestAnimationFrame(function(){
-		animate();
-	});
-}
-
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-//create camera/set
-var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-//set position
-
-//starting at center point 
-camera.position.y= -400; //y position, new and far
-camera.position.z= 400; //left and right
-camera.rotation.x= 45*(Math.PI/180); //up and down
-// 45 degrees 
-
-var scene = new THREE.Scene(); //creat scene
-
-//normal material for mesh, creating cube
-//cube has x,y,z = all set to 200
-var cube  = new THREE.Mesh(new THREE.CubeGeometry(200,200,200), new THREE.MeshLambertMaterial({
-	color: '#EE00FF',
+	    for (var i=0; i<100; i++){
+	    	var mesh = new THREE.Mesh( geometry, material);
+	    	//position for each cube x,y and z
+	    	//subtrack by 0.5 to get som negatice numbers
+	    	mesh.position.x = (Math.random()-0.5) * 2000;
+	    	mesh.position.z = (Math.random()-0.5) * 2000;
+	    	mesh.position.y = (Math.random()-0.5) * 2000;
 
 
-}));
+	    	mesh.position.x = (Math.random()-0.5) * 2000;
+	    	mesh.position.z = (Math.random()-0.5) * 2000;
+	    	mesh.position.y = (Math.random()-0.5) * 2000;
 
-//creating light
-var light = new THREE.DirectionalLight("white");
-light.position.set(0,-400,400).normalize(); //position light
-scene.add(light); //adding light to scene
+	    	mesh.rotation.x = (Math.random()-0.5);
+	    	mesh.rotation.z = (Math.random()-0.5);
+	    	mesh.rotation.y = (Math.random()-0.5);
 
 
-//rotating cube 
-cube.rotation.z = 45*(Math.PI/180);
-//adding plane to scene
-scene.add(cube);
+	   		scene.add(mesh);
+	   		//put cubes in different x locations
 
-//calling function
-animate();
+	    }
+
+
+
+
+	    renderer = new THREE.WebGLRenderer();
+	    renderer.setSize(window.innerWidth, window.innerHeight)
+	    document.body.appendChild(renderer.domElement);
+	}
+
+	function animate(){
+	    requestAnimationFrame( animate );
+	    controls.update();
+	}
+	function render(){
+	    renderer.render( scene, camera );
+	} 
 };
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
